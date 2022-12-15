480cd246b04675d7e33f8c85006f6c148d3285eb
LAPTOP-NDTF2MRT\jiank <cjk7072@closeli.cn>
2022-12-15 22:05:35 +0800

    rename title

--- .vitepress/NavItems.ts
+++ .vitepress/NavItems.ts
@@ -1,113 +1,113 @@
-import { DefaultTheme } from "vitepress";
+import { DefaultTheme } from 'vitepress';
 
 export const NavItemList: DefaultTheme.NavItem[] = [
     {
-        text: "前端基础",
+        text: '前端基础',
         items: [
             {
-                text: "HTML&CSS",
-                link: "/JavaScript/HTML&CSS/",
+                text: 'HTML&CSS',
+                link: '/JavaScript/HTML&CSS/',
             },
             {
-                text: "函数式编程",
-                link: "/JavaScript/Functional/",
+                text: '函数式编程',
+                link: '/JavaScript/Functional/',
             },
             {
-                text: "JS异步编程",
-                link: "/JavaScript/Asynchronous/",
+                text: 'JS异步编程',
+                link: '/JavaScript/Asynchronous/',
             },
             {
-                text: "ES新特性",
-                link: "/JavaScript/ESFeatures/",
+                text: 'ES新特性',
+                link: '/JavaScript/ESFeatures/',
             },
             {
-                text: "TS语言",
-                link: "/JavaScript/TypeScript/",
+                text: 'TS语言',
+                link: '/JavaScript/TypeScript/',
             },
             {
-                text: "JS性能优化",
-                link: "/JavaScript/Performance/",
+                text: 'JS性能优化',
+                link: '/JavaScript/Performance/',
             },
             {
-                text: "项目工程化",
-                link: "/JavaScript/Engineering/",
+                text: '项目工程化',
+                link: '/JavaScript/Engineering/',
             },
             {
-                text: "模块检测",
-                link: "/JavaScript/Module/",
+                text: '模块检测',
+                link: '/JavaScript/Module/',
             },
         ],
     },
     {
-        text: "前端框架",
+        text: '前端框架',
         items: [
             {
-                text: "Vue",
-                link: "/Framework/Vue/Vue2",
+                text: 'Vue',
+                link: '/Framework/Vue/Vue2',
             },
             {
-                text: "React",
-                link: "/Framework/React/React",
+                text: 'React',
+                link: '/Framework/React/React',
             },
             {
-                text: "Nuxt",
-                link: "/Framework/Nuxt/serveRender",
+                text: 'Nuxt',
+                link: '/Framework/Nuxt/serveRender',
             },
             {
-                text: "Next",
-                link: "/Framework/Next/NextJS",
+                text: 'Next',
+                link: '/Framework/Next/NextJS',
             },
             {
-                text: "杂记",
-                link: "/Framework/Module/",
+                text: '杂记',
+                link: '/Framework/Module/',
             },
         ],
     },
     {
-        text: "全栈开发",
+        text: '全栈开发',
         items: [
             {
-                text: "Node",
-                link: "/FullStack/Node/WebCli",
+                text: 'Node',
+                link: '/FullStack/Node/WebCli',
             },
             {
-                text: "NoSQL数据库",
-                link: "/FullStack/DataBase/Mongodb",
+                text: 'NoSQL数据库',
+                link: '/FullStack/DataBase/Mongodb',
             },
             {
-                text: "Web开发框架",
-                link: "/FullStack/WebDev/Express",
+                text: 'Web开发框架',
+                link: '/FullStack/WebDev/Express',
             },
             {
-                text: "GraphQL API 开发",
-                link: "/FullStack/GraphQL/",
+                text: 'GraphQL API 开发',
+                link: '/FullStack/GraphQL/',
             },
         ],
     },
     {
-        text: "泛客户端开发",
+        text: '泛客户端开发',
         items: [
             {
-                text: "React Native",
-                link: "/PanClient/RN/WebCli",
+                text: 'React Native',
+                link: '/PanClient/RN/WebCli',
             },
             {
-                text: "Flutter",
-                link: "/PanClient/RN/WebCli",
+                text: 'Flutter',
+                link: '/PanClient/RN/WebCli',
             },
         ],
     },
     {
-        text: "技术方案专题",
+        text: '技术方案',
         items: [
             {
-                text: "Micro Frontend",
-                link: "/ProgramTopics/MicroFrontend/WebCli",
+                text: 'Micro Frontend',
+                link: '/ProgramTopics/MicroFrontend/WebCli',
             },
         ],
     },
     {
-        text: "宝藏网站",
-        link: "/Sites",
+        text: '宝藏网站',
+        link: '/Sites',
     },
 ];
